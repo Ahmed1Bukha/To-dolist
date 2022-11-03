b0043a80b9576915cc19c6b929900df6f25affb3
Ahmed_Bukha <xxbu7aamedxx@gmail.com>
2022-11-03 10:42:59 +0300

    did the statmanagment

--- todoist/lib/widgets/list_tile.dart
+++ todoist/lib/widgets/list_tile.dart
@@ -1,15 +1,47 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
+class listTileLmao extends StatefulWidget {
+  const listTileLmao({super.key});
 
+  @override
+  State<listTileLmao> createState() => _listTileLmaoState();
+}
 
+class _listTileLmaoState extends State<listTileLmao> {
+  bool isChecked = false;
 
+  void isCheckedCallBack() {
+    setState(() {
+      isChecked = !isChecked;
+    });
+  }
 
-  ListTile listTileLmao() {
+  @override
+  Widget build(BuildContext context) {
     return ListTile(
-      title: Text("This is a task"),
-      trailing: Checkbox(
-        value: false,
-        onChanged: (value) {},
+      title: Text(
+        "This is a task",
+        style: TextStyle(
+            decoration:
+                isChecked ? TextDecoration.lineThrough : TextDecoration.none),
       ),
+      trailing: taskChecBox(isChecked, isCheckedCallBack),
     );
   }
+}
+
+class taskChecBox extends StatelessWidget {
+  final bool isChecked;
+  final Function isCheckedCallBack;
+  taskChecBox(this.isChecked, this.isCheckedCallBack);
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      value: isChecked,
+      onChanged: (value) => isCheckedCallBack(),
+    );
+  }
+}
--- todoist/lib/widgets/list_view.dart
+++ todoist/lib/widgets/list_view.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 import 'list_tile.dart';
+import 'package:todoist/modules/task.dart';
+
 class viewLmfao extends StatelessWidget {
+  List<Task> task = [
+    Task(name: "Take the trash out."),
+    Task(name: "Lmfao gg ff go next"),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return ListView(
@@ -8,6 +15,4 @@ class viewLmfao extends StatelessWidget {
       children: [listTileLmao(), listTileLmao()],
     );
   }
-
-
 }
